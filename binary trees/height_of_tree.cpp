4b377616bf722ec419bcd4f10261a684b0bcc964
Dhiraj <dhirajdurgade7758@gmail.com>
2025-08-25 19:36:13 +0530

    iterative inorder and height of tree

---
+++ binary trees/height_of_tree.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* left;
+    Node* right;
+    
+    Node(int data){
+        this->data = data;
+        this->left = nullptr;
+        this->right = nullptr;
+    }
+};
+
+void inorder(Node* root){
+    stack<Node*> s;
+    Node* node = root;
+    while(true){
+        if(node!=nullptr){
+            s.push(node);
+            node=node->left;
+        }
+        else{
+            if(s.empty()) break;
+            node=s.top();
+            s.pop();
+            cout<<node->data<<"-";
+            node=node->right;
+        }
+    }
+}
+
+int height(Node* root){
+    if(root==nullptr){
+        return 0;
+    }
+    int left = height(root->left);
+    int right = height(root->right);
+    
+    int currht = max(left, right) + 1;
+    return currht;
+}
+
+int main()
+{
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    
+    root->left->left= new Node(4);
+    root->left->right = new Node(5);
+    
+    root->left->right->left = new Node(8);
+    inorder(root);
+    cout<<endl;
+    cout<<"height of tree is "<<height(root);
+return 0;
+}
