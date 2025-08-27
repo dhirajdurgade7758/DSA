b1196a21a8e23b5aae44cc2b149a1066e30a795a
Dhiraj <dhirajdurgade7758@gmail.com>
2025-08-27 18:05:30 +0530

    diameter of a treee

---
+++ binary trees/diameter_of_tree.cpp
@@ -0,0 +1,88 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* left;
+    Node* right;
+    
+    Node(int data){
+        this->data = data;
+        this->left = nullptr;
+        this->right = nullptr;
+    }
+};
+
+void inorder(Node* root){
+    stack<Node*> s;
+    Node* node = root;
+    while(true){
+        if(node!=nullptr){
+            s.push(node);
+            node=node->left;
+        }
+        else{
+            if(s.empty()) break;
+            node=s.top();
+            s.pop();
+            cout<<node->data<<"-";
+            node=node->right;
+        }
+    }
+}
+
+int height(Node* root){
+    if(root==nullptr){
+        return 0;
+    }
+    int left = height(root->left);
+    int right = height(root->right);
+    
+    int currht = max(left, right) + 1;
+    return currht;
+}
+
+int diam(Node* root){
+    if(root == nullptr){
+        return 0;
+    }
+    
+    int currDiam = height(root->left) + height(root->right) + 1;
+    int leftDiam = diam(root->left);
+    int rightDiam = diam(root->right);
+    if(currDiam >= leftDiam){
+        if(currDiam >= rightDiam){
+            return currDiam;
+        }
+        else{
+            return rightDiam;
+        }
+    }
+    else{
+        if(leftDiam >= rightDiam){
+            return leftDiam;
+        }
+        else{
+            return leftDiam;
+        }
+    }
+}
+
+int main()
+{
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    
+    root->left->left= new Node(4);
+    root->left->right = new Node(5);
+    
+    root->left->right->left = new Node(8);
+    inorder(root);
+    cout<<endl;
+    cout<<"height of tree is "<<height(root)<<endl;
+    cout<<"diameter of a tree "<<diam(root);
+return 0;
+}
