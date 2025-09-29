7a5fe973d4e91de5af45fd98a41ae0cc37afcaa0
Dhiraj <dhirajdurgade7758@gmail.com>
2025-09-29 11:27:15 +0530

    lcs rec

--- Dynamic Programing/minCostToCutStickTab.cpp
+++ Dynamic Programing/minCostToCutStickTab.cpp
@@ -18,6 +18,24 @@ public:
         cuts.push_back(n);
         cuts.insert(cuts.begin(), 0);
         sort(cuts.begin(), cuts.end());
-        return f(1, c, cuts);
+
+        vector<vector<long long>> dp(c+2, vector<long long>(c+2, 0));
+
+        for(int i=c; i>=1; i--){
+            for(int j=1; j<=c; j++){
+                if(i>j){
+                    continue;
+                }
+                long long mini = INT_MAX;
+
+                for(int ind=i; ind<=j; ind++){
+                    long long cost = cuts[j+1] - cuts[i-1] + dp[i][ind-1] + dp[ind+1][j];
+                    mini = min(mini, cost);
+                }
+                dp[i][j]= mini;
+
+            }
+        }
+        return dp[1][c];
     }
 };
