8ee2e4ebd4a0802120fa93479e5c17a11c7f7b63
Dhiraj <dhirajdurgade7758@gmail.com>
2025-11-08 22:49:14 +0530

    diagonal_sum revision

--- 2D matrix chapter/diagonal_sum.cpp
+++ 2D matrix chapter/diagonal_sum.cpp
@@ -8,33 +8,35 @@ using namespace std;
         int sum = 0;
 
         //O(n^2) time complexity
-        // for(int i=0; i<n; i++){
+        //      for(int i=0; i<n; i++){
         //     for(int j=0; j<n; j++){
         //         if(i == j){
-        //             sum += mat[i][j];
+        //             sum += mat[i][j]; // primary diagonal
         //         }
         //         else if(j == n-i-1){
-        //             sum += mat[i][j];
+        //             sum += mat[i][j]; // secondary diagonal
         //         }
         //     }
         // }
 
+
         //O(n) time complexity
-        for(int i=0; i<n; i++){
-            sum += mat[i][i];
-            if(i != n-i-1){
-                sum += mat[i][n-i-1];
-            }
-        }
+        // for(int i=0; i<n; i++){
+        // sum += mat[i][i];           // primary diagonal
+        // if(i != n-i-1){             // avoid double counting middle element
+        //     sum += mat[i][n-i-1];   // secondary diagonal
+        // }
+        // }
+
         return sum;
     }
 
 int main(){
 
     vector<vector<int>> mt = {
-        {1,2,3,4},
-        {5,6,7,8},
-        {9,10,11,12}
+         {1,2,3},
+        {4,5,6},
+        {7,8,9}
     };
 
     cout<<"Digonal sum of the 2D matrix is "<<diagonalSum(mt);
