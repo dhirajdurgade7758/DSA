4d08ed6dcadd5d612ce4a0473a30bd39b1237c28
Dhiraj <dhirajdurgade7758@gmail.com>
2025-11-09 11:05:02 +0530

    max subarray sum 1

---
+++ Arrays/maxSubArrraySum1.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+void maxSubArraySum1(int *arr, int n){
+    int maxSum=INT_MIN;
+    for(int start=0; start<n; start++){
+        for(int end=start; end<n; end++){
+            int currSum=0;
+            for (int i=start; i<=end; i++)
+            {
+                currSum+=arr[i];
+            }
+            maxSum = max(maxSum, currSum);
+        }
+    }
+    cout<<"maximum subarray sum is: "<<maxSum<<endl;
+}
+
+int main()
+{
+    int arr[] = {1,2,3,4,5};
+    int n=5;
+
+    maxSubArraySum1(arr, n);
+
+
+return 0;
+}
