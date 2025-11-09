081660d7f9559972ad415e0860fc98d56d213f24
Dhiraj <dhirajdurgade7758@gmail.com>
2025-11-09 08:27:05 +0530

    revised inverted patterns

--- patterns questions/inverted_patterns/p1.cpp
+++ patterns questions/inverted_patterns/p1.cpp
@@ -1,21 +1,3 @@
-// #include<iostream>
-// using namespace std;
-
-// int main(){
-//     int n = 5;
-//     int i,j;
-//     for(i=0;i<n;i++){
-//         for(j=0;j<n;j++){
-//             if(j<=i){
-//                 cout<<" ";
-//             }
-//             else{
-//                 cout<<i+1;
-//             }
-//         }
-//         cout<<endl;
-//     }
-// }
 
 #include<iostream>
 using namespace std;
