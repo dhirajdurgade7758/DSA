382ffe368aa52fa211018425385a6e2d3f11efa3
Dhiraj <dhirajdurgade7758@gmail.com>
2025-11-18 17:30:01 +0530

    remove duplicates from sorted array

---
+++ Arrays/removeDuplicatesFromSortedArray.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+    int removeDuplicates(vector<int>& nums) {
+        if(nums.size() == 0){
+            return 0;
+        }
+        int k=1;
+        for(int i=1; i<nums.size(); i++){
+            if(nums[i] != nums[i-1]){
+                k++;
+            }
+        }
+        return k;
+    }
+
+int main()
+{
+    vector<int> nums = {0,0,1,1,1,2,2,3,3,4};
+    cout<<"unique values from arrays are: "<<removeDuplicates(nums)<<endl;
+return 0;
+}
