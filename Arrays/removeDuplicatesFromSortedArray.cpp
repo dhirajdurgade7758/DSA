c912b55cee8686597df01bcff5002230b7c58c8e
Dhiraj <dhirajdurgade7758@gmail.com>
2025-11-20 13:00:25 +0530

    remove duplicate update

--- Arrays/removeDuplicatesFromSortedArray.cpp
+++ Arrays/removeDuplicatesFromSortedArray.cpp
@@ -7,12 +7,14 @@ using namespace std;
         if(nums.size() == 0){
             return 0;
         }
-        int k=1;
+        int k=0;
         for(int i=1; i<nums.size(); i++){
             if(nums[i] != nums[i-1]){
                 k++;
+                nums[k] = nums[i];
             }
         }
+        k++;
         return k;
     }
 
