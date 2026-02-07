8d893d01e251f8f7fd9abcacc29947421bcea2fe
Dhiraj <dhirajdurgade7758@gmail.com>
2026-02-08 00:14:26 +0530

    tower of hanoi

--- Recursion (part 2)/Q4.cpp
+++ Recursion (part 2)/Q4.cpp
@@ -1,14 +1,32 @@
 #include<iostream>
 using namespace std;
 
-int towerOfHanoi(int n) {
-    if (n == 0) {
-        return 0; // No moves needed for zero disks
+  void towerOfHanoiHelper(int n, int from, int to, int aux, int& count){
+        if(n==1){
+            cout<<"move 1 disk from "<<from<<" to "<<to<<endl;
+            count++;
+            return;
+        }
+        
+        towerOfHanoiHelper(n-1, from,to,aux,count);
+        cout<<"move 1 disk from "<<from<<" to "<<to<<endl;
+        count++;
+        towerOfHanoiHelper(n-1, aux,to,from,count);
+        
     }
-    return 2 * towerOfHanoi(n - 1) + 1; // Recursive relation
-}
+    
+    
+    int towerOfHanoi(int n, int from, int to, int aux) {
+        // code here
+        int count =0;
+        towerOfHanoiHelper(n,from,to, aux,count);
+        return count;
+    }
 
 int main() {
-    cout << "Number of moves required for 3 disks: " << towerOfHanoi(3) << endl;
+    int from=1;
+    int to=3;
+    int aux=2;
+    cout << "Number of moves required for 3 disks: " << towerOfHanoi(3, from, to, aux) << endl;
     return 0;
 }
