6fce3a39729d82382c0c11e3367ebbb80c54cffe
Dhiraj <dhirajdurgade7758@gmail.com>
2026-03-04 22:44:33 +0530

    refactor mergeTriplets function and update problem summary for clarity

--- greedy algorithums/medium/mergeTriplets.cpp
+++ greedy algorithums/medium/mergeTriplets.cpp
@@ -1,24 +1,4 @@
-/*
-PROBLEM: Merge Triplets to Form Target Triplet
-===============================================
-Given a list of triplets and a target triplet, determine if you can merge some 
-triplets from the list to form the target triplet.
 
-Rules:
-- A triplet can be selected if all its values are less than or equal to the 
-  corresponding target values (s[i] <= target[i] for all i).
-- From valid triplets, take the maximum value at each position.
-- If the resulting triplet equals the target, return true; otherwise, return false.
-
-Example:
-    triplets = [[2,5,3],[1,8,4],[1,7,5]]
-    target = [2,7,5]
-    Output: true
-    Explanation: Merge [1,7,5] and [2,5,3] to get [max(1,2), max(7,5), max(5,3)] = [2,7,5]
-
-Time Complexity: O(n) where n is the number of triplets
-Space Complexity: O(1)
-*/
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -26,17 +6,17 @@ using namespace std;
 class Solution {
 public:
     bool mergeTriplets(vector<vector<int>>& triplets, vector<int>& target) {
-        vector<int> res(3, INT_MIN);
-
-        for (auto s : triplets) {
-            if (s[0] <= target[0] && s[1] <= target[1] && s[2] <= target[2]) {
-                res[0] = max(res[0], s[0]);
-                res[1] = max(res[1], s[1]);
-                res[2] = max(res[2], s[2]);
+        bool a=false;
+        bool b=false;
+        bool c=false;
+        for(auto s:triplets){
+            if(s[0] <= target[0] && s[1] <= target[1] && s[2] <= target[2]){
+                if(s[0] == target[0]) a=true;
+                if(s[1] == target[1]) b=true;
+                if(s[2] == target[2]) c=true;
             }
         }
-
-        return res == target;
+        return a && b && c;
     }
 };
 
