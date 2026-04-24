b618c64b4402368e3cd8aac29565c88cbcd5eb44
Dhiraj <dhirajdurgade7758@gmail.com>
2026-04-25 02:30:58 +0530

    Add commented implementations and problem summaries for MyQueue and MyStack classes

--- Stacks & Queues & heaps/Queue/Medium/queue_using2Stacks.cpp
+++ Stacks & Queues & heaps/Queue/Medium/queue_using2Stacks.cpp
@@ -101,6 +101,75 @@ class Queue{
     }
 };
 
+// class MyQueue {
+// public:
+//     stack<int> s1, s2;
+//     MyQueue() {
+        
+//     }
+    
+//     void push(int data) {
+//         s1.push(data);
+//     }
+    
+//     int pop() {
+//         if (s1.empty())
+//         {
+//             cout<<"Queue is empty! "<<endl;
+//             return -1;
+//         }
+
+//         while (!s1.empty())
+//         {
+//             s2.push(s1.top());
+//             s1.pop();
+//         }
+//         int poped=s2.top();
+//         s2.pop();
+
+//         while (!s2.empty())
+//         {
+//             s1.push(s2.top());
+//             s2.pop();
+//         }
+//         return poped;
+//     }
+    
+//     int peek() {
+//         if (s1.empty())
+//         {
+//             cout<<"Queue is empty! "<<endl;
+//             return -1;
+//         }
+
+//         while (!s1.empty())
+//         {
+//             s2.push(s1.top());
+//             s1.pop();
+//         }
+//         int front = s2.top();
+
+//         while (!s2.empty())
+//         {
+//             s1.push(s2.top());
+//             s2.pop();
+//         }
+//         return front;
+//     }
+    
+//     bool empty() {
+//         return s1.empty();
+//     }
+// };
+
+// /**
+//  * Your MyQueue object will be instantiated and called as such:
+//  * MyQueue* obj = new MyQueue();
+//  * obj->push(x);
+//  * int param_2 = obj->pop();
+//  * int param_3 = obj->peek();
+//  * bool param_4 = obj->empty();
+//  */
 int main()
 {
     Queue obj;
--- Stacks & Queues & heaps/Queue/Medium/stack_using2Queues.cpp
+++ Stacks & Queues & heaps/Queue/Medium/stack_using2Queues.cpp
@@ -36,8 +36,57 @@ class Stack{
     bool empty(){
         return q1.empty();
     }
-};
+// };
+// class MyStack {
+// public:
+//     queue<int> q1,q2;
+//     MyStack() {
+        
+//     }
+    
+//     void push(int data) {
+//         while (!q1.empty())
+//         {
+//             q2.push(q1.front());
+//             q1.pop();
+//         }
+//         q1.push(data);
+//         while (!q2.empty())
+//         {
+//             q1.push(q2.front());
+//             q2.pop();
+//         }
+//     }
+    
+//     int pop() {
+//         int front=q1.front();
+//         q1.pop();
+//         return front;
+        
+//     }
+    
+//     int top() {
+//         if(q1.empty()){
+//             cout<<"stack is empty! "<<endl;
+//             return -1;
+//         }
 
+//         return q1.front();
+//     }
+    
+//     bool empty() {
+//         return q1.empty();
+//     }
+// };
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
 int main()
 {
     Stack obj;
